76fde380c5badf618cccf560c5f06043d3d86273
Lorenzo Sacchi <saccunzia@gmail.com>
2019-07-07 09:40:50 +0100

    first steps in actual interface

--- blackjack.py
+++ blackjack.py
@@ -34,9 +34,9 @@ def testoutput():
 	AllDeck = deck.Deck()
 	AllDeck.mix()
 	#create Player1
-	Player1 = players.player('unzia',100)
+	#Player1 = players.player('unzia',100)
 	#ask to hit. name is tied to deck name
-	Player1.hit(AllDeck)
+	#Player1.hit(AllDeck)
 	
 	#print(Player1.hand[0].outfit())
 	#Player1.hit(AllDeck)
@@ -54,19 +54,29 @@ def testoutput():
 		#print(AllDeck.complete_deck[i].value_name, 'of' ,AllDeck.complete_deck[i].sign_name)
 		#print(AllDeck.complete_deck[i].value, 'of' ,AllDeck.complete_deck[i].sign)
 	#decide card number. create a hand with first card down. must be part of player and cpu methods
-	cardnumber = 2
-	hand = []
-	for i in range(cardnumber):
-		hand.append(AllDeck.popcard())
+	#cardnumber = 2
+	#hand = []
+	#for i in range(cardnumber):
+		#hand.append(AllDeck.popcard())
 	
-	print(hand[0].outfit_card_down())
+	#print(hand[0].outfit_card_down())
 	#print(hand[0].value_name, 'of' ,hand[0].sign_name)
-	for k in range(1,cardnumber):
-		print(hand[k].outfit())
+	#for k in range(1,cardnumber):
 		#print(hand[k].outfit())
-		print(hand[k].value_name, 'of' ,hand[k].sign_name)
+		#print(hand[k].outfit())
+		#print(hand[k].value_name, 'of' ,hand[k].sign_name)
 		#print(hand[k].value)
 		#print(hand[k].number)
+	
+	#Cpu hand
+	#Cpu = players.cpu()
+	#Cpu.hit(AllDeck)
+	#print(Cpu.hand[0].outfit())
+
+	# Maybe put bet and hit as requests on the main, no object needed
+
+	Interface = game.interface()
+	print(Interface.top)
 
 
 
--- gamelbs/game.py
+++ gamelbs/game.py
@@ -2,13 +2,21 @@ class interface:
 	"""
 	defone the genertal outline of the objects in the game
 	"""
-	
+	def __init__(self):
+		self.top = '-----   blackjack   -----'
+
+		
+
 class game_status:
 	"""
 	get the values based on the cards on screen.
 	elaborate game's score and present it.
 	present winner
 	"""
+	def __init__(self):
+		pass
+
+
 
 class game_interaction:
 	"""
@@ -17,6 +25,10 @@ class game_interaction:
 		bet
 		hit/stay
 	"""
+	def __init__(self):
+		pass
+
+
 
 class new_game:
 	"""
@@ -25,14 +37,27 @@ class new_game:
 		load
 		restart
 		exit
+		acquire user name, amount
+		remember tu destroy
 	"""
+	def __init__(self, username, starting_amount):
+		self.username = username
+		self.starting_amount = starting_amount
+
+
 
 class bet:
 	"""
 	bet at the beginning of the game
 	"""
+	def __init__(self):
+		pass
+
+
 
 class gamble:
 	"""
 	ask to hit or stay
 	"""
+	def __init__(self):
+		pass
